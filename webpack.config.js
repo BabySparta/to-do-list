71fb422229f38721c7c706fe4c208c9a2c534fd7
Brandon <brandon.r.young@outlook.com>
2022-08-03 18:28:31 -0400

    add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
