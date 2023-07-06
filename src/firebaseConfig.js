96e6c61f5d97285e19f11fa1be08941b50bb7d31
BabySparta <123456789+odin@users.noreply.github.com>
2023-07-06 19:18:42 -0400

    Add firebase support

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKDphBrAGWe2XRdjDdVXoS54A9NSxaeAo",
+  authDomain: "to-do-list-b17c6.firebaseapp.com",
+  projectId: "to-do-list-b17c6",
+  storageBucket: "to-do-list-b17c6.appspot.com",
+  messagingSenderId: "99417419867",
+  appId: "1:99417419867:web:5458b5151ea3f0341d8f7e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
